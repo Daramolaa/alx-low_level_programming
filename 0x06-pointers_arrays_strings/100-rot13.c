7d0b7c53748b9db91ca721584e22d02631f607b1
Daramolaa <Oluwadaramoladada@gmail.com>
2022-07-14 16:16:36 -0700

    TASK 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *rot13 - rotate characters 13 places in the alphabet
+ * @s: string
+ * Return: Converted string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char base[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; base[j] != '\0'; j++)
+			if (s[i] == base[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+	}
+
+	return (s);
+}
